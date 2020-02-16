ebff8f54fe035f9f43674fc44cc22eda248a1d64
Merwan <achibet.merwan@gmail.com>
2020-02-16 14:19:42 +0100

    Add imgui-rs

--- psxtest/src/main.rs
+++ psxtest/src/main.rs
@@ -2,11 +2,31 @@ extern crate psx;
 
 use psx::psx::PSX; // TODO rename to System or something
 
+use imgui::*;
 use std::env;
 use std::path::Path;
 
+mod support;
+
 fn main()
 {
+    let system = support::init(file!());
+    system.main_loop(move |_, ui| {
+        Window::new(im_str!("Hello world"))
+            .size([300.0, 100.0], Condition::FirstUseEver)
+            .build(ui, || {
+                ui.text(im_str!("Hello world!"));
+                ui.text(im_str!("こんにちは世界！"));
+                ui.text(im_str!("This...is...imgui-rs!"));
+                ui.separator();
+                let mouse_pos = ui.io().mouse_pos;
+                ui.text(format!(
+                    "Mouse Position: ({:.1},{:.1})",
+                    mouse_pos[0], mouse_pos[1]
+                ));
+            });
+    });
+
     // Load the BIOS
 
     let args: Vec<String> = env::args().collect();
